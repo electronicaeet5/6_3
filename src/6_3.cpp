a0c44e7bafcffe16a52ca71221789fed96c081c0
yo <yo@gmail.com>
2024-03-29 13:58:11 -0300

    Ejemplo funcional c++

--- src/6_3.cpp
+++ src/6_3.cpp
@@ -1,8 +1,11 @@
 #include "6_3.h"
+#include <algorithm>
+#include <iostream>
+#define CANTIDAD_NUMEROS 10
 
 int16_t leerdato()
 {
-    static constexpr int16_t valores[] = {7,    56,     255,    -56,    1256,
+    static constexpr int16_t valores[] = {7,    56,     2555,   -56,    1256,
                                           125,  125,    -2200,  14,     458,
                                           -789, 7745,   0,      -56,    0};
     static uint8_t i = 0;
@@ -13,5 +16,11 @@ int16_t leerdato()
 
 int main()
 {
+    auto max=leerdato();
+    for(uint8_t i=0;i<CANTIDAD_NUMEROS-1;i++)
+    {
+        max=std::max(max,leerdato());
+    }
+    std::cout << "el maximo es " << max << std::endl;
     return 0;
 }
